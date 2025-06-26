38f82da4baaa569ad9bf74dbf451020989c05ab5
tynnn696 <dinad2960@example.com>
2025-06-26 14:43:48 +0800

    Hapus Semua Node

--- antrian pelayanan bank.cpp
+++ antrian pelayanan bank.cpp
@@ -1,14 +1,8 @@
-void tampilkanAntrian() {
-        if (front == nullptr) {
-            cout << "Antrian kosong.\n";
-            return;
-        }
-
-        Node* temp = front;
-        cout << "Daftar antrian nasabah:\n";
-        int no = 1;
-        while (temp != nullptr) {
-            cout << no++ << ". " << temp->nama << endl;
-            temp = temp->next;
+~AntrianBank() {
+        while (front != nullptr) {
+            Node* temp = front;
+            front = front->next;
+            delete temp;
         }
     }
+};
